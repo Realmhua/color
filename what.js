263a95f2218955fe938842e3f7b579f1c65ef8af
guohuawx <guohua74@126.com>
2013-08-30 11:15:31 +0800

    another summer day

---
+++ what.js
@@ -0,0 +1,8 @@
+If you've found yourself on this page, we're assuming you're brand new to Git and GitHub.
+This guide will walk you through the basics and explain a little bit about how everything works along
+the way.
+
+Download the Install Git
+At the heart of GitHub is an open source version control system (VCS) called Git*. Created by the same
+team that created Linux, Git is responsible for everything GitHub related that happens locally on your computer.
+
